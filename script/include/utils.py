a3d32e796c184feb300a313915595f3dc8c4a18c
Everett Toews <everett.toews@rackspace.com>
2015-10-29 16:01:08 -0500

    Log warnings (and only warnings) from all scripts

--- script/include/utils.py
+++ script/include/utils.py
@@ -1,5 +1,12 @@
 import logging
 
+class MyFilter(object):
+    def __init__(self, level):
+        self.__level = level
+
+    def filter(self, logRecord):
+        return logRecord.levelno == self.__level
+
 def log_warnings():
     handler = logging.FileHandler('warnings.log')
 
@@ -8,6 +15,8 @@ def log_warnings():
     logger = logging.getLogger('py.warnings')
     logger.setLevel(logging.WARNING)
     logger.addHandler(handler)
+
+    handler.addFilter(MyFilter(logging.WARNING))
 
     logger = logging.getLogger('novaclient.api_versions')
     logger.setLevel(logging.WARNING)
